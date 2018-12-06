5184ade0271995233f8eb82496079dafa4413d21
Ayaz Gulmammadov <gulmammadov.ayaz@gmail.com>
2018-12-06 10:35:49 +0400

    Add AzureStorageAccount.ps1 script

---
+++ Task1/AzureStorageAccount.ps1
@@ -0,0 +1,25 @@
+﻿param(
+    [Parameter(Mandatory)][string]$SubscriptionName,
+    [Parameter(Mandatory)][string]$StorageAccountName,
+    [Parameter(Mandatory)][string]$ResGroup,
+    [Parameter(Mandatory)][string]$Location,
+    [Parameter(Mandatory)][string]$ContainerName,
+    [Parameter(Mandatory)][string]$FilesToUpload
+    )
+# Add your Azure account to the local Powershell environment
+Add-AzureRmAccount
+
+# If we don't have a resource group, we have to create one
+New-AzureRmResourceGroup -Name $ResGroup -Location $Location
+
+# Create a new storage account
+New-AzureRmStorageAccount -StorageAccountName $StorageAccountName -Location $Location -ResourceGroupName $ResGroup -SkuName Standard_LRS
+
+# Set a current storage account
+Set-AzureRmCurrentStorageAccount -ResourceGroupName $ResGroup -Name $StorageAccountName
+
+# Create a new container
+New-AzureStorageContainer -Name $ContainerName -Permission Off
+
+# Upload files into a container
+Get-ChildItem -File $FilesToUpload -Recurse | Set-AzureStorageBlobContent -Container $ContainerName 

bc2cd0d1b27257b71d0c44d2485f8dba0fcb1b17
Ayaz Gulmammadov <gulmammadov.ayaz@gmail.com>
2018-12-07 19:08:40 +0400

    Add AzureAutoLogon.ps1

---
+++ Task3/AzureAutoLogon.ps1
@@ -0,0 +1,8 @@
+﻿<#
+After logging into Azure, save the credentials in a file that you can use in the future in the PowerShell script for the logon process.
+
+#>
+param(
+    [Parameter(Mandatory)][string]$Path
+)
+Login-AzureRmAccount | Save-AzureRmContext -Path $Path

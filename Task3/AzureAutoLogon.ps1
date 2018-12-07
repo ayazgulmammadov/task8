2dbb5a37227909894464056f054fe4050985f6c7
Ayaz Gulmammadov <gulmammadov.ayaz@gmail.com>
2018-12-07 19:22:49 +0400

    remove empty space

--- Task3/AzureAutoLogon.ps1
+++ Task3/AzureAutoLogon.ps1
@@ -1,6 +1,5 @@
 ﻿<#
 After logging into Azure, save the credentials in a file that you can use in the future in the PowerShell script for the logon process.
-
 #>
 param(
     [Parameter(Mandatory)][string]$Path
